42f1de7560a34f55dfce95d35afd674a0b7ed0a5
Renantx <renan.tkdunb@gmail.com>
2021-09-01 11:21:28 -0300

    commit inicial

---
+++ livrariaAPI/Models/todoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace livrariaAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> option)
+             : base(option)
+        {
+        }
+
+        public DbSet<Product> TodoProducts { get; set; }
+    }
+}
